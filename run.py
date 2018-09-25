4122497bdea0f2c818d9819bd2a003ddff362b71
MSallermann <M.Sallermann@fz-juelich.de>
2018-09-25 15:55:45 +0200

    Added CalcJ and CalcReducedField

--- run.py
+++ run.py
@@ -1,16 +1,9 @@
-path_to_spirit_pkg = "/Users/sallermann/Coding/spirit/core/python"
+path_to_spirit_pkg = "/home/moritz/Coding/spirit/core/python"
 import sys
 sys.path.append(path_to_spirit_pkg)
 
 import numpy as np
 from spirit import state, simulation, constants, parameters, parameters, geometry, system, hamiltonian, configuration, io
-
-size = 5
-n_it = 10000
-J = 0.084
-Q = 0
-
-fields = np.arange(1, 3, step = 0.1)
 
 def getVorticity(spins, size):
     result = 0
@@ -22,11 +15,34 @@ def getVorticity(spins, size):
         result += (np.dot(spins[size-1 + b * size + size * size * (size-1)], [0,  1, 0]  ))
     return result / (4 * size)
 
+def calcReducedField(field):
+    return field * (mu_0 * mu_B * 1E30)
+
+def calcJ(size, lam):
+    return (size * 1E-10)**2 / lam**2 * (mu_0 * (mu_B * 1E30)**2)/2 * 1E-10
+
+
+mu_B = 0.057883817555
+mu_0 = 2.0133545*1e-28
+
+size = 5
+n_it = 10000
+Q = 0
+lam = 10
+J = calcJ(size, lam)
+
+fields = np.arange(1, 3, step = 1)
+
+
+
+
 
 with open('output.txt', 'a') as out:
     out.write("#Q = {0}\n".format(Q))
+    out.write("#J = {0}\n".format(J))
+    out.write("#lambda = {0}\n".format(lam))
     out.write("#n_it = {0}\n".format(n_it))
-    out.write("#size, ext field, reduced_field, J, vorticity\n")
+    out.write("#size, ext field, reduced_field, vorticity\n")
 
     with state.State("") as p_state:
         parameters.llg.set_output_general(p_state, any=False)
@@ -46,10 +62,10 @@ with open('output.txt', 'a') as out:
             simulation.start(p_state, simulation.METHOD_LLG, simulation.SOLVER_VP, n_iterations = n_it)
             spins = np.array(system.get_spin_directions(p_state)).reshape(nos, 3)
 
-            vorticity = np.abs(getVorticity(spins, size))
-            print(vorticity)
-            out.write("{0}, {1}, {2}\n".format(size, field, vorticity))
+            reduced_field = calcReducedField(field)
 
-            # io.chain_write(p_state, "image" + str(field))
+
+            vorticity = np.abs(getVorticity(spins, size))
+            out.write("{0}, {1}, {2}, {3}\n".format(size, field, reduced_field, vorticity))
 
 
